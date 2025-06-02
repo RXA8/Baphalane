cca5a2dfa68ab67c45537fc2aa5275d6029f55d5
RXA8 <u19029269@tuks.co.za>
2025-06-02 12:16:42 +0200

    Updated Council page

--- baphalane/src/app/council/page.tsx
+++ baphalane/src/app/council/page.tsx
@@ -85,7 +85,7 @@ export default function CouncilPage() {
       <section id="council" className="bg-gray-200 py-12">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-semibold text-center text-gray-800 mb-10">
-            Meet Our Council
+            Traditional Council Members
           </h2>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
